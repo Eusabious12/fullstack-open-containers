443fe05a988fde722b1fb8bc30728b028bd3682f
Eusabious12 <your-github-email@example.com>
2026-09-26 14:40:37 +0300

    Exercise 17

--- todo-app/todo-frontend/vite.config.js
+++ todo-app/todo-frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['localhost', 'app'],
+  },
   test: {
     environment: 'jsdom',
     globals: true,
